c94ccb85cf466112360b2ece36fdf63cb655ee65
Desiki-high <2448906309@qq.com>
2023-03-20 13:04:23 +0800

    增加注释

--- convert/convert.py
+++ convert/convert.py
@@ -14,6 +14,9 @@ def exit(status):
 
 class Image:
     def __init__(self, source_registry, insecure_source_registry, target_registry, insecure_target_registry, image, prefetch=""):
+        """
+        the prefetch is the file path of prefetch list file,and it is optional
+        """
         self.source_registry = source_registry
         self.insecure_source_registry = insecure_source_registry
         self.target_registry = target_registry
@@ -53,6 +56,10 @@ class Image:
                 return f"cat {self.prefetch} | nydusify convert --prefetch-patterns --source {self.source_registry}/{self.image} --target {self.target_registry}/{target_image}"
 
     def convert(self):
+        """
+        convert oci image to nydus image (prefetchfile is optional)
+        """
+        print(self.convert_cmd())
         rc = os.system(self.convert_cmd())
         assert rc == 0
 
