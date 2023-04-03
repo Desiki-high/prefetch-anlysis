5f3fc8023f384af14a439bb38f211beb7e643ddd
dingyadong <ding_yadong@foxmail.com>
2023-04-03 16:06:35 +0800

    update

--- anlysis_filesize.py
+++ anlysis_filesize.py
@@ -10,6 +10,10 @@ def main():
     def classify(x):
         if x <= 1024:
             return "<1K"
+        if x <= 16 * 1024:
+            return "~16K"
+        if x <= 32 * 1024:
+            return "~32K"
         if x <= 64 * 1024:
             return "~64K"
         if x <= 128 * 1024:
@@ -26,7 +30,7 @@ def main():
             return "~4M"
         return ">4M"
 
-    order = ["<1K", "~64K", "~128K", "~256K", "~512K", "~1M", "~2M", "~4M", ">4M"]
+    order = ["<1K", "~16K", "~32K", "~64K", "~128K", "~256K", "~512K", "~1M", "~2M", "~4M", ">4M"]
     df["c_file_size"] = df["file_size"].apply(classify)
 
     # data = df.groupby("c_file_size")["file_size"].sum()
@@ -34,6 +38,11 @@ def main():
     data = df["c_file_size"].value_counts().reindex(order)
 
     plt.bar(data.index, data.values)
+
+    # Add labels to the top of each bar
+    for i, v in enumerate(data.values):
+        plt.text(i, v+10, str(v), ha='center')
+
     plt.savefig("file_size.png")
 
 
