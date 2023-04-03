af7902ffec6da607ca5a31598ab02d6aa782faa8
dingyadong <ding_yadong@foxmail.com>
2023-04-03 16:02:09 +0800

    add 64k

--- anlysis_filesize.py
+++ anlysis_filesize.py
@@ -5,31 +5,33 @@ import pandas as pd
 
 
 def main():
-    df = pd.read_csv('data/wordpress:php8.2_nydus/2023-03-28-15:30:35.csv')
+    df = pd.read_csv("data/wordpress:php8.2_nydus/2023-03-28-15:30:35.csv")
 
     def classify(x):
         if x <= 1024:
-            return '<1K'
+            return "<1K"
+        if x <= 64 * 1024:
+            return "~64K"
         if x <= 128 * 1024:
-            return '~128K'
+            return "~128K"
         if x <= 256 * 1024:
-            return '~256K'
+            return "~256K"
         if x <= 512 * 1024:
-            return '~512K'
+            return "~512K"
         if x <= 1024 * 1024:
-            return '~1M'
+            return "~1M"
         if x <= 2 * 1024 * 1024:
-            return '~2M'
+            return "~2M"
         if x <= 4 * 1024 * 1024:
-            return '~4M'
-        return '>4M'
+            return "~4M"
+        return ">4M"
 
-    order = ['<1K', '~128K', '~256K', '~512K', '~1M', '~2M', '~4M', '>4M']
-    df['c_file_size'] = df['file_size'].apply(classify)
+    order = ["<1K", "~64K", "~128K", "~256K", "~512K", "~1M", "~2M", "~4M", ">4M"]
+    df["c_file_size"] = df["file_size"].apply(classify)
 
-    # data = df.groupby('c_file_size')['file_size'].sum()
+    # data = df.groupby("c_file_size")["file_size"].sum()
 
-    data = df['c_file_size'].value_counts().reindex(order)
+    data = df["c_file_size"].value_counts().reindex(order)
 
     plt.bar(data.index, data.values)
     plt.savefig("file_size.png")
